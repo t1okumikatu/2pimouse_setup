3e3e947da99e2d74146f1c3e6b0a860c3134892e
“t1okumikatu” <t1okumikatu@gmail.com>
2019-11-14 06:44:50 +0000

    first commit 11-14

--- setup.bash
+++ setup.bash
@@ -1,18 +1,13 @@
 #!/bin/bash -xve
-<<<<<<< HEAD
-#Written by yohsi     
-=======
-#Written by yoshi
-     
->>>>>>> 2ccd6c82c5ca88c56355ef64caa1df899a46a4d5
+
 exec 2> /tmp/setup.log
-     
+
 cd /home/ubuntu/RaspberryPiMouse/src/drivers/
 /sbin/insmod rtmouse.ko
-     
+
 sleep 1                   #すぐにはデバイスファイルができないので待つ
 chmod 666 /dev/rt*
-     
+
 echo 0 > /dev/rtmotoren0  #安全のためモータの電源を切っておく
 
 # Copyright 2016 Ryuichi Ueda
